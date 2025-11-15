86ff206849caadb20fcf0a4ed1c646c1331edf21
Basma Youssry <basmayoussry17@gmail.com>
2025-11-15 15:52:53 +0200

    Create a menu slider

--- src/app/features/home/components/main-slider/main-slider.component.ts
+++ src/app/features/home/components/main-slider/main-slider.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
-Swiper.use([Navigation]);
+Swiper.use([Navigation, Pagination]);
+
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import { SwiperOptions } from 'swiper/types';
+import { FlowbiteService } from '../../../../core/services/flowbite.service';
+import { initFlowbite } from 'flowbite';
+
 @Component({
   selector: 'app-main-slider',
   imports: [],
@@ -13,5 +16,35 @@ import { SwiperOptions } from 'swiper/types';
   styleUrl: './main-slider.component.css'
 })
 export class MainSliderComponent {
+   constructor(private flowbiteService: FlowbiteService) {}
 
+  ngOnInit(): void {
+    this.flowbiteService.loadFlowbite((flowbite) => {
+      initFlowbite();
+    });
+  }
+  // ngAfterViewInit(): void {
+  //   setTimeout(() => {
+  //   const swiper = new Swiper('.mySwiper', {
+  //       modules: [Navigation, Pagination],
+  //       slidesPerView: 1,
+  //       spaceBetween: 0,
+  //       loop: true,
+
+  //       navigation: {
+  //         nextEl: '.swiper-button-next',
+  //         prevEl: '.swiper-button-prev',
+  //       },
+
+  //       pagination: {
+  //         el: '.swiper-pagination',
+  //         clickable: true,
+  //         dynamicBullets: true,
+  //         dynamicMainBullets: 2,
+  //       },
+  //     });
+  //   });
+  // }
+
+  
 }
